5597d42a8d26a8c1eeb9db89eb9f41a2db931204
dalmia <amandalmia18@gmail.com>
2025-12-24 09:47:33 +0530

    added logs for startup

--- src/startup.py
+++ src/startup.py
@@ -6,6 +6,7 @@ from api.config import UPLOAD_FOLDER_NAME
 root_dir = os.path.dirname(os.path.abspath(__file__))
 
 if __name__ == "__main__":
+    print("Setting up...")
     asyncio.run(init_db())
 
     # create uploads folder
@@ -13,3 +14,5 @@ if __name__ == "__main__":
         upload_folder = os.path.join(root_dir, UPLOAD_FOLDER_NAME)
         if not os.path.exists(upload_folder):
             os.makedirs(upload_folder)
+
+    print("Setup complete")
